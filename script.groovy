40ca07c110859a1e5965c910f979da7f61d1c59e
Jolly Omere <47526890+J-kings@users.noreply.github.com>
2023-05-12 21:07:23 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docke-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t jollyomere/demo:app-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push jollyomere/demo:app-2.0'
